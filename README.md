cd9353b26447a1a3d5b9d5aef234122e7d8b1932
YosafatM <yosafat_martinez21@hotmail.com>
2021-11-01 19:33:04 -0600

    Migración a Flutter 2.0

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # UFlutter-destini
 Séptima aplicación en Flutter
+Actualización a Flutter 2.0
 
 Esta aplicación forma parte del curso de Flutter: https://www.udemy.com/course/flutter-bootcamp-with-dart/
--- lib/story.dart
+++ lib/story.dart
@@ -1,5 +1,9 @@
 class Story {
   String storyTitle, choice1, choice2;
 
-  Story({this.storyTitle, this.choice1, this.choice2});
+  Story({
+    required this.storyTitle,
+    required this.choice1,
+    required this.choice2
+  });
 }
